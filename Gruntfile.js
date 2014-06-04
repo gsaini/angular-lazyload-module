ce8994a1f54348ae9692eb625e78c994e62101ff
gsaini <gopal.saini.work@gmail.com>
2014-06-04 21:17:00 +0530

    Module wise build

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    // Displays the execution time of [grunt](http://gruntjs.com) tasks
+    require('time-grunt')(grunt);
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        requirejs: {
+            compile: {
+                options: {
+                    dir: "build",
+					removeCombined: true,
+					mainConfigFile:'public/modules/main.js'
+                }
+            }
+        },
+        bower: {
+            target: {
+                rjsConfig: 'public/modules/main.js',
+                options: {
+                    baseUrl: 'public/'
+                }
+            }
+        }
+    });
+    grunt.registerTask('build', ['requirejs']);
+};
